26515ae59e41b26dbde0f3e4b55b9403ed3f5b39
iceflarexd <nikangel04@gmail.com>
2022-05-10 01:45:44 -0500

    Add project files.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CSharpOsu;
+using CSharpOsu.Module;
+
+namespace TotalPpCalc
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public OsuClient osu = new OsuClient(Api.apikey);
+        public OsuUserBest[] userScores;
+        public OsuUser[] player;
+        public List<float> userTopPps;
+        public float totalWithBonus, bonusPp;
+        float totalPp;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (e.LeftButton == MouseButtonState.Pressed) DragMove();
+        }
+
+        private void CloseWindow(object sender, RoutedEventArgs e)
+            => Close();
+
+        private void MinWindow(object sender, RoutedEventArgs e)
+            => this.WindowState = WindowState.Minimized;
+
+        private void OnGoClick(object sender, RoutedEventArgs e)
+        {
+            try 
+            {
+                userScores = osu.GetUserBest(userIdText.Text, 0, 100);
+                userTopPps = new List<float>();
+
+                player = osu.GetUser(userIdText.Text);
+
+                foreach (var user in player ) usernameText.Text = user.username;
+
+                foreach (var score in userScores)
+                {
+                    userTopPps.Add(score.pp);
+                }
+
+                totalPp = 0;
+                for (int i = 0; i < 100; i++)
+                {
+                    totalPp += (float)(userTopPps[i] * Math.Pow(0.95, i));
+                }
+
+                bonusPp = CalcBonusPp(totalPp);
+
+                totalPpText.Text = totalPp.ToString();
+                bonusPpText.Text = (totalPp + bonusPp).ToString();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void OnRecalcClick(object sender, RoutedEventArgs e)
+        {
+            userTopPps.Sort();
+            userTopPps.Reverse();
+            totalPp = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                totalPp += (float)(userTopPps[i] * Math.Pow(0.95, i));
+            }
+
+            totalPpText.Text = totalPp.ToString();
+            bonusPpText.Text = (totalPp + bonusPp).ToString();
+        }
+
+        private void OnAddScoreClick(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                userTopPps.Add(Int32.Parse(newPpText.Text));
+                newPpText.Clear();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private float CalcBonusPp(float totalPp)
+        {
+            foreach (var user in player) totalWithBonus = user.pp_raw;
+
+            return totalWithBonus - totalPp;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+# Total Weighted PP Calculator for osu!
+![](https://i.imgur.com/yEJkTD9.png)
+
+Enter new top plays and see how much weighted PP it would give you.
+
+(Example: If you wanted to see how many more 300pp plays you'd need to get 5000pp weighted PP)
