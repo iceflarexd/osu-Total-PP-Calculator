5e4da7733cb5fcdf680e7e63153959b5b81532cd
iceflarexd <nikangel04@gmail.com>
2022-05-10 02:03:04 -0500

    quick optimizations to code

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using CSharpOsu;
+﻿using CSharpOsu;
 using CSharpOsu.Module;
+using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Input;
 
 namespace TotalPpCalc
 {
@@ -47,14 +37,14 @@ namespace TotalPpCalc
 
         private void OnGoClick(object sender, RoutedEventArgs e)
         {
-            try 
+            try
             {
                 userScores = osu.GetUserBest(userIdText.Text, 0, 100);
                 userTopPps = new List<float>();
 
                 player = osu.GetUser(userIdText.Text);
 
-                foreach (var user in player ) usernameText.Text = user.username;
+                foreach (var user in player) usernameText.Text = user.username;
 
                 foreach (var score in userScores)
                 {
